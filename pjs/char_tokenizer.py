6d7e1b8133d86c173808eb8c7d2a94ef78e58b49
LaurensWink <laurensherzebrock@googlemail.com>
2025-09-04 13:42:32 +0200

    temporary debug_tok.txt

--- pjs/char_tokenizer.py
+++ pjs/char_tokenizer.py
@@ -2,7 +2,7 @@ import os
 import string
 from tokenizers import Tokenizer, models
 from tokenizers.normalizers import Lowercase, Sequence, Replace, NFC
-from transformers import PreTrainedTokenizerFast
+from transformers import AutoTokenizer, PreTrainedTokenizerFast
 from tokenizers.pre_tokenizers import Split
 from tokenizers.processors import TemplateProcessing
 
@@ -52,15 +52,37 @@ def gen_char_tokenizer():
     os.makedirs("data/tokenizer/char_tokenizer", exist_ok=True)
     wrapped_tokenizer.save_pretrained("data/tokenizer/char_tokenizer")
 
-gen_char_tokenizer()
+# gen_char_tokenizer()
 
 char_tokenizer = PreTrainedTokenizerFast.from_pretrained("data/tokenizer/char_tokenizer")
 
-text = "Überraschung! Das ist überraschend"
-
 
 # Test
-text = "Überraschung! Das ist überraschend"
+text = "Überraschung! 1234567890 Das ist überraschend"
+out = char_tokenizer(text, add_special_tokens=True)  # Special Token am Anfang
+
+print("=== Input IDs & Tokens ===")
+for idx in out["input_ids"]:
+    token = char_tokenizer.convert_ids_to_tokens(idx)
+    print(f"{idx:5}  {token!r}")
+
+decoded_text = char_tokenizer.decode(out["input_ids"], skip_special_tokens=False )
+print("\n=== Decoded Text ===")
+print(decoded_text)
+
+# Prüfen, welche Special Tokens der Tokenizer kennt
+print("\n=== Special Tokens ===")
+print("All Special Tokens:", char_tokenizer.all_special_tokens)
+print("All Special Token IDs:", char_tokenizer.all_special_ids)
+print("BOS Token:", char_tokenizer.bos_token, "ID:", char_tokenizer.bos_token_id)
+print("EOS Token:", char_tokenizer.eos_token, "ID:", char_tokenizer.eos_token_id)
+print("PAD Token:", char_tokenizer.pad_token, "ID:", char_tokenizer.pad_token_id)
+print("UNK Token:", char_tokenizer.unk_token, "ID:", char_tokenizer.unk_token_id)
+
+char_tokenizer =  AutoTokenizer.from_pretrained('phonemetransformers/babble-tokenizers', subfolder='BABYLM-TOKENIZER-CHAR-TXT')
+
+# Test
+text = "Überraschung! 1234567890 Das ist überraschend"
 out = char_tokenizer(text, add_special_tokens=True)  # Special Token am Anfang
 
 print("=== Input IDs & Tokens ===")
